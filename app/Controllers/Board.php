56481b0342baae104e26e1bc0d9a6c67a0654746
chen <chenhoyoung@gmail.com>
2025-10-08 00:06:31 +0900

    8강 model 이용

--- app/Controllers/Board.php
+++ app/Controllers/Board.php
@@ -2,14 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Models\BoardModel;//사용할 모델을 반드시 써줘야한다.
+
 class Board extends BaseController
 {
     public function list()
     {
-        $db = db_connect();
-        $query = "select * from board order by bid desc";
-        $rs = $db->query($query);
-        $data['list'] = $rs->getResult();//결과값 저장
+        // $db = db_connect();
+        // $query = "select * from board order by bid desc";
+        // $rs = $db->query($query);
+        // $data['list'] = $rs->getResult();//결과값 저장
+        $boardModel = new BoardModel();
+        $data['list'] = $boardModel->orderBy('bid', 'DESC')->findAll();
+
         return render('board_list', $data);//view에 리턴        
         //return render('board_list');  
         //return view('board_list');
